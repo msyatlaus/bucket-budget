8c1340e7e2e5b5e77517c9cc8c5229b759c7eb1d
nbur4556 <nbur4556@gmail.com>
2020-10-25 22:14:12 -0500

    changes text values to string values in users model

--- models/users.js
+++ models/users.js
@@ -2,9 +2,9 @@ const Sequelize = require('sequelize');
 const connection = require('../config/connection');
 
 const users = connection.define('users', {
-    username: Sequelize.TEXT,
-    pin_hash: Sequelize.TEXT,
-    salt: Sequelize.TEXT
+    username: Sequelize.STRING,
+    pin_hash: Sequelize.STRING,
+    salt: Sequelize.STRING
 });
 
 module.exports = users;
