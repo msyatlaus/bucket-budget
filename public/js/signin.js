a394f6e22ccca755c1e7df656708273d4de456f2
nbur4556 <nbur4556@gmail.com>
2020-10-27 15:28:03 -0500

    includes google sign in functions

--- public/js/signin.js
+++ public/js/signin.js
@@ -4,9 +4,16 @@ $(document).ready(function () {
         gapi.auth2.init();
     });
 
-    $('#g-signin').click(function () {
-        alert("google signin");
-    });
+    function onSignIn(googleUser) {
+        // var profile = googleUser.getBasicProfile();
+        // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        // console.log('Name: ' + profile.getName());
+        // console.log('Image URL: ' + profile.getImageUrl());
+        // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+        console.log("test Button");
+    }
+
 
     $("#signin").click(function () {
         var userName = $("#username").val();
