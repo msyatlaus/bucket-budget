0447e397dc23a3fa862f58c6ff316c52af282fc0
nbur4556 <nbur4556@gmail.com>
2020-10-26 20:32:11 -0500

    budget items api logs information

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,9 @@
+const controller = require('../controllers/bucket-budget-controller.js');
+
 module.exports = function (app) {
-    app.get('/api/budgetItems', (res, req) =>{
-        
+    app.get('/api/budgetItems', (res, req) => {
+        controller.getFromBudgetItems(data => {
+            console.log(data);
+        });
     });
 }
