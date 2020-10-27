e5c1436bb1680830d4372cc3f5cb16c3e2a69bb1
nbur4556 <nbur4556@gmail.com>
2020-10-26 21:12:03 -0500

    send json to budgetItems api

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("public"));
+
 require('./routes/api-routes')(app);
 
 app.listen(PORT, () => {
-    console.log("server running on localhost: ", PORT);
+    console.log("server running on localhost:", PORT);
 });
 
 // const axios = require('axios');
