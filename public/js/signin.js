1a01e33b44718dc5cc0b6d4567f0dd7ace34249b
nbur4556 <nbur4556@gmail.com>
2020-10-27 19:12:33 -0500

    signing in gets google user information

--- public/js/signin.js
+++ public/js/signin.js
@@ -3,17 +3,24 @@ $(document).ready(function () {
     gapi.load('auth2', () => {
         gapi.auth2.init({
             client_id: '320381748790-3v1689ebkinvmndl34kjb07fu2nijvl7.apps.googleusercontent.com'
-        }).then(() => {
-            console.log("Google auth initialized");
-
-            // Sign in button on click
-            $('.g-signin2').click(() => {
-                // console.log('test');
-
-                console.log(gapi.auth2);
-            })
         });
     });
+
+    // Sign in button on click
+    $('.g-signin2').click(() => {
+        const auth2 = gapi.auth2.getAuthInstance();
+        auth2.signIn().then(() => {
+            const profile = auth2.currentUser.get().getBasicProfile();
+            console.log(profile);
+            console.log('ID: ' + profile.getId());
+            console.log('Full Name: ' + profile.getName());
+            console.log('Given Name: ' + profile.getGivenName());
+            console.log('Family Name: ' + profile.getFamilyName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail());
+
+        });
+    })
 
     $("#signin").click(function () {
         var userName = $("#username").val();
