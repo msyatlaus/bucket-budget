3ae3253f2ee8236cc0ff0a7c4e9b709a30a5920a
nbur4556 <nbur4556@gmail.com>
2020-10-27 19:36:46 -0500

    google signin sends ajax post to backend

--- public/js/signin.js
+++ public/js/signin.js
@@ -11,16 +11,20 @@ $(document).ready(function () {
         const auth2 = gapi.auth2.getAuthInstance();
         auth2.signIn().then(() => {
             const profile = auth2.currentUser.get().getBasicProfile();
-            console.log(profile);
-            console.log('ID: ' + profile.getId());
-            console.log('Full Name: ' + profile.getName());
-            console.log('Given Name: ' + profile.getGivenName());
-            console.log('Family Name: ' + profile.getFamilyName());
-            console.log('Image URL: ' + profile.getImageUrl());
-            console.log('Email: ' + profile.getEmail());
 
+            $.ajax({
+                method: 'POST',
+                url: '/api',
+                data: {
+                    userId: 'ID: ' + profile.getId(),
+                    email: 'Email: ' + profile.getEmail(),
+                    firstName: 'Given Name: ' + profile.getGivenName(),
+                    lastName: 'Family Name: ' + profile.getFamilyName(),
+                    imgUrl: 'Image URL: ' + profile.getImageUrl()
+                }
+            });
         });
-    })
+    });
 
     $("#signin").click(function () {
         var userName = $("#username").val();
