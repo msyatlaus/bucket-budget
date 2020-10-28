dd4ce96d45325ac736d9e459dce86502ee42ff06
nbur4556 <nbur4556@gmail.com>
2020-10-27 20:38:19 -0500

    created api call to triposo

--- routes/triposo-routes.js
+++ routes/triposo-routes.js
@@ -1,13 +1,16 @@
 const axios = require('axios');
+const config = require('../config/config.js');
 
 module.exports = function (app) {
     app.get('/api/triposo/:city', (req, res) => {
         let city = req.params.city;
 
-        axios.get(`https://www.triposo.com/api/20200803/tour.json?location_ids=${city}&count=10&account=${config.tripsoApiId}&token=${config.tripsoApiKey}`)
+        axios.get(`https://www.triposo.com/api/20200803/tour.json?location_ids=${city}&count=10&account=${config.triposo.apiId}&token=${config.triposo.apiKey}`)
             .then(data => {
                 console.log(data.data.results);
                 res.json(data.data.results);
+            }).catch(err => {
+                console.log(err);
             });
     });
 }
--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-const planning = require('planning');
-
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
