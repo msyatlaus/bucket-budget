f37c15e8aec62e0b415479cee09746fa04b85a7f
nbur4556 <nbur4556@gmail.com>
2020-10-27 20:17:41 -0500

    sets up triposo routes file

---
+++ routes/triposo-routes.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+module.exports = function (app) {
+
+}
+
+// AXIOS TESTING
+// const axios = require('axios');
+
+// axios.get(`https://www.triposo.com/api/20200803/location.json?id=Tokyo&account=${config.tripsoApiId}&token=${config.tripsoApiKey}`)
+//     .then(data => {
+//         console.log(data.data.results[0]);
+//     });
+
+// axios.get(`https://www.triposo.com/api/20200803/tour.json?location_ids=${planning.citySearch}&count=10&account=${config.tripsoApiId}&token=${config.tripsoApiKey}`)
+//     .then(data => {
+//         console.log(data.data.results);
+//     });
