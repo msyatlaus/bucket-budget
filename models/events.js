48c0b00743288ce2e1cf1a687aa8bc33021285da
Cooper <eliteswimmer42@gmail.com>
2020-10-29 18:58:04 -0500

    updated events.js

--- models/events.js
+++ models/events.js
@@ -3,8 +3,11 @@ const connection = require('../config/connection');
 
 const events = connection.define('events', {
     name: Sequelize.STRING,
+    quantity: Sequelize.INTEGER,
+    price: Sequelize.INTEGER,
     description: Sequelize.TEXT,
-    date_time: Sequelize.DATE
+    date_time: Sequelize.DATE,
+    event_id: Sequelize.STRING
 });
 
 module.exports = events;
