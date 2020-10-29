5c4b59b494e9ddbeae69bbe210c5dbe9f55e1450
Shubhangi Mundhada <shubhimundhada@gmail.com>
2020-10-29 10:06:05 -0500

    Adding in the header for Hotel

--- public/js/planning.js
+++ public/js/planning.js
@@ -1,18 +1,19 @@
-$(document).ready(function () {
-    // $("#search").click(function () {
-    //     var city = $("#placeVisit").val();
-    //     $.ajax({
-    //         method: "GET",
-    //         url: "/api/triposo/" + city
-    //     }).then(function (data) {
-    //         console.log(data);
-    //         var resultEl = document.getElementById("result");
-    //         resultEl.style.display = "block";
-    //     });
-    // });
 
+$(document).ready(function () {
     $("#search").click(function () {
-        var resultEl = document.getElementById("result");
-         resultEl.style.display = "block";
+        var city = $("#placeVisit").val();
+        $.ajax({
+            method: "GET",
+            url: "/triposo/highlights/hotels/" + city
+        }).then(function (data) {
+            console.log(data);
+            var resultEl = document.getElementById("result");
+            resultEl.style.display = "block";
+        });
     });
+
+    // $("#search").click(function () {
+    //     var resultEl = document.getElementById("result");
+    //      resultEl.style.display = "block";
+    // });
 });
