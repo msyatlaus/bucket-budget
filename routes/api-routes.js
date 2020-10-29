ff167ab46afe266f1f615083f53b9a185e8b5454
Cooper <eliteswimmer42@gmail.com>
2020-10-28 19:27:09 -0500

    fixed up some minor typos

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,9 +1,7 @@
 const controller = require("../controllers/bucket-budget-controller.js");
 
-// refer to passport.js -- boiler plate code in case we go down the email/password authentication route rather than google sign-in
-// const passport = require('../config/passport');
-
 module.exports = function (app) {
+    
     app.get('/api/budgetItems', (req, res) => {
         controller.getFromBudgetItems(data => {
             console.log(data[0].dataValues);
