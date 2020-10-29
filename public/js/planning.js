affab7725faf21820e7e5e6a639843c9313a3751
Shubhangi Mundhada <shubhimundhada@gmail.com>
2020-10-29 10:41:00 -0500

    Populated the hotel table with data

--- public/js/planning.js
+++ public/js/planning.js
@@ -9,6 +9,15 @@ $(document).ready(function () {
             console.log(data);
             var resultEl = document.getElementById("result");
             resultEl.style.display = "block";
+            $.each(data, function(i, item) {  
+                $('#hotels').append($('<tr>').attr('id',$.trim(decodeURIComponent(item.id))).append(
+                             $('<td>').text($.trim(decodeURIComponent(item.name))),
+                             $('<td>').text($.trim(decodeURIComponent(item.hotels_score))),
+                             $('<td>').text($.trim(decodeURIComponent(((item.hotels_score)/2)*100)))                    
+                           )
+                )
+            });
+            
         });
     });
 
