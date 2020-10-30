86ffc2b86e80e53150b240b709afd92108de30ea
nbur4556 <nbur4556@gmail.com>
2020-10-29 19:25:09 -0500

    removed uneeded columns from event model

--- models/events.js
+++ models/events.js
@@ -3,11 +3,8 @@ const connection = require('../config/connection');
 
 const events = connection.define('events', {
     name: Sequelize.STRING,
-    quantity: Sequelize.INTEGER,
-    price: Sequelize.INTEGER,
     description: Sequelize.TEXT,
-    date_time: Sequelize.DATE,
-    event_id: Sequelize.STRING
+    date_time: Sequelize.DATE
 });
 
 module.exports = events;
