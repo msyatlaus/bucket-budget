2ac72e8cd93ab1883d371bee85ffc21f4b894d6c
nbur4556 <nbur4556@gmail.com>
2020-10-29 21:32:38 -0500

    created seed file for shopping items

---
+++ db/shopping-items-seed.sql
@@ -0,0 +1,271 @@
+USE budget_bucket_db;
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Shirts",
+    "Clothing",
+    20,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Trousers",
+    "Clothing",
+    35,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Jeans",
+    "Clothing",
+    50,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Sweater",
+    "Clothing",
+    60,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Delicates",
+    "Clothing",
+    25,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Socks",
+    "Clothing",
+    12,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Pyjamas",
+    "Clothing",
+    30,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Shorts",
+    "Clothing",
+    20,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Swimwear",
+    "Clothing",
+    13,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Belt",
+    "Clothing",
+    25,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Gloves",
+    "Clothing",
+    20,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Shampoo",
+    "Toiletries",
+    14,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Tooth Brush",
+    "Toiletries",
+    4,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Mouth Wash",
+    "Toiletries",
+    4,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Hair Gel",
+    "Toiletries",
+    15,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Hand Sanitizer",
+    "Toiletries",
+    5,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Shaving Kit",
+    "Toiletries",
+    15,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Lotion",
+    "Toiletries",
+    8,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Contact Lens",
+    "Toiletries",
+    150,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Sunscreen",
+    "Toiletries",
+    3,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Pain Reliever",
+    "Medication / First Aid",
+    4,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Cough/Cold Medicine",
+    "Medication / First Aid",
+    10,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Band-Aid",
+    "Medication / First Aid",
+    3,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Insect Repellent",
+    "Medication / First Aid",
+    5,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Sunglasses",
+    "Miscellaneous",
+    100,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Alarm",
+    "Miscellaneous",
+    5,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Electrical Converter",
+    "Miscellaneous",
+    30,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Umbrella",
+    "Miscellaneous",
+    5,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Pillow",
+    "Miscellaneous",
+    7,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
+INSERT INTO
+  shopping_items(name, category, price, createdAt, updatedAt)
+VALUES(
+    "Laundry Bag",
+    "Miscellaneous",
+    3,
+    "10-29-2020 09:30:00",
+    "10-29-2020 09:30:00"
+  );
