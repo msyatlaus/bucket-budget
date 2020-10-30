4dbee770deeca447aab9025e180e3be352322875
Cooper <eliteswimmer42@gmail.com>
2020-10-29 19:24:14 -0500

    updated controllers

--- controllers/bucket-budget-controller.js
+++ controllers/bucket-budget-controller.js
@@ -13,6 +13,8 @@ db.synchronize();
 
 const Controller = function () { }
 
+
+// Budget Items
 Controller.prototype.getFromBudgetItems = function (cb) {
   db.budgetItems.findAll().then(data => {
     cb(data);
@@ -47,7 +49,58 @@ Controller.prototype.deleteBudgetItems = function (listItem, cb) {
   });
 }
 
+
+// Events
+Controller.prototype.getFromEvents = function (cb) {
+  db.events.findAll().then(data => {
+    cb(data);
+  });
+}
+
+Controller.prototype.createEvents = function (listItem, cb) {
+  db.events.create(listItem).then(data => {
+    cb(data);
+  });
+}
+
+Controller.prototype.updateEvents = function (listItem, cb) {
+  db.events.update(
+    listItem.body,
+    {
+      where: {
+        id: listItem.id
+      }
+    }).then(data => {
+    cb(data);
+  });
+}
+
+Controller.prototype.deleteEvents = function (listItem, cb) {
+  db.events.destroy({
+    where: {
+      id: listItem
+    }
+  }).then(data => {
+    cb(data);
+  });
+}
+
 const controller = new Controller();
+
+// controller.createEvents({
+//   name: "test",
+//   description: "testing",
+// });
+
+// controller.updateEvents({
+//   body: {
+//     name: "test",
+//     description: "testing",
+//   },
+//   id: 3
+// });
+
+// controller.deleteEvents(2);
 
 // Export routes for server.js to use.
 module.exports = controller;
